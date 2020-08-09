fe744723065851db85285714a3f434a52fb895d0
FWao <44507699+FWao@users.noreply.github.com>
2020-08-09 19:45:24 +0200

    Update register.py

--- register.py
+++ register.py
@@ -8,7 +8,7 @@ import http.client
 
 
 deviceId = '' # set uuid like d5494cc8-9c52-11ea-bb37-0242ac130002
-url = '' # https://CLOUD.MYCLOUD.XYZ/index.php/apps/sensorlogger/api/v1/registerdevice/ -> NOTE: SLASH AT THE END is nescessary
+url = '' # https://CLOUD.MYCLOUD.XYZ/index.php/apps/sensorlogger/api/v1/registerdevice/ -> NOTE: SLASH AT THE END is necessary
 
 def registerDevice():
 
@@ -17,7 +17,7 @@ def registerDevice():
     payload = {
         'deviceId': deviceId,
         'deviceName': 'Sensor#1',
-        'deviceType': 'Indoor',
+        'deviceType': 'IndoorTempHumid',
         'deviceGroup': 'living room',
         'deviceDataTypes': [
             {
@@ -42,14 +42,13 @@ def registerDevice():
 
     http = urllib3.PoolManager()
 
-    credentials = '%s:%s' % ('USERNAME', 'DEVICE TOKEN')
+    credentials = '%s:%s' % ('USERNAME', 'DEVICE PASSWORD')
     encoded_credentials = base64.b64encode(credentials.encode('ascii'))
 
     r = http.request('POST', url, headers={'Content-Type': 'application/json', "Content-Security-Policy" : "default-src 'none';script-src 'self' 'unsafe-eval';style-src 'self' 'unsafe-inline';img-src 'self' data: blob:;font-src 'self';connect-src 'self';media-src 'self'", "Authorization" : "Basic %s" % encoded_credentials.decode('ascii')},
              body=encoded_body)
 
-    # uncomment to print http response
-    # print(r.data)
+    print(r.data)
 
 
 registerDevice()
